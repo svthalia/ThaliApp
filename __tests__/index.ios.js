2a89fe18e673e29ae0c1ed0dc829a7c9f90da3b4
Wietse Kuipers <wietse@kuiprs.nl>
2017-02-15 16:08:55 +0100

    Added test coverage report

--- __tests__/index.ios.js
+++ __tests__/index.ios.js
@@ -3,7 +3,7 @@ import React from 'react';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-import Index from '../app/containers/app';
+import Index from '../app/app';
 
 it('renders correctly', () => {
   const tree = renderer.create(
