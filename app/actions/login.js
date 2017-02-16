ea6d675629714fe7aae23d67d787d63892c3ca8c
Wietse Kuipers <wietse@kuiprs.nl>
2017-02-16 15:27:35 +0100

    async login example

--- app/actions/login.js
+++ app/actions/login.js
@@ -1,8 +1,24 @@
 import * as types from './actionTypes';
 
-export function login(username, password) {
+export function loginold(username, password) {
   return {
     type: types.LOGIN,
     success: password === '42',
+  };
+}
+
+export function login(username, password) {
+  return (dispatch) => {
+    return fetch('https://thalia.nu/api/login', {
+      method: 'POST',
+    })
+      .then(
+        response => response.json())
+      .then(
+        (responseJson) => {
+          console.log(responseJson);
+          return dispatch(loginold(username, password));
+        })
+      .catch(error => console.error(error));
   };
 }
