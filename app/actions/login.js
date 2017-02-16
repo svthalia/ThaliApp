b5a66a631da9363f44ea5a14bd2cc84f68bf4d15
Wietse Kuipers <wietse@kuiprs.nl>
2017-02-16 14:01:27 +0100

    Change error to warning to make action exporting more consistent

--- app/actions/login.js
+++ app/actions/login.js
@@ -1,6 +1,6 @@
 import * as types from './actionTypes';
 
-export default function login(username, password) {
+export function login(username, password) {
   return {
     type: types.LOGIN,
     success: password === '42',
