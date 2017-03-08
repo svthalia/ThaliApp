64b4b83a418c8fdbf81ed581ac8e02da317eb1e3
Gijs Hendriksen <gijs@hendrxx.nl>
2017-03-09 00:21:42 +0100

    Added state storage, implemented basic logout functionality

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { BackAndroid } from 'react-native';
+import React, { Component } from 'react';
+import { BackAndroid, AsyncStorage } from 'react-native';
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
@@ -7,10 +7,14 @@ import thunk from 'redux-thunk';
 import * as reducers from './reducers';
 import ReduxNavigator from './components/navigator';
 import { back } from './actions/navigation';
+import { initLogin } from './actions/login';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 const reducer = combineReducers(reducers);
 const store = createStoreWithMiddleware(reducer);
+
+const USERNAMEKEY = '@MyStore:username';
+const TOKENKEY = '@MyStore:token';
 
 BackAndroid.addEventListener('hardwareBackPress', () => {
   if (store.getState().navigation.previousScenes.length === 0) {
@@ -20,10 +24,34 @@ BackAndroid.addEventListener('hardwareBackPress', () => {
   return true;
 });
 
-const Main = () =>
-  <Provider store={store}>
-    <ReduxNavigator />
-  </Provider>
-;
+class Main extends Component {
+  componentDidMount() {
+    AsyncStorage.multiGet([USERNAMEKEY, TOKENKEY])
+      .then(
+        (result) => {
+          // Transform [key, value] pairs into object, for easier access
+          const values = result.reduce((obj, pair) => {
+            const obj2 = { ...obj };
+            obj2[pair[0]] = pair[1];
+            return obj2;
+          }, {});
+
+          const username = values[USERNAMEKEY];
+          const token = values[TOKENKEY];
+
+          if (username !== null && token !== null) {
+            store.dispatch(initLogin(username, token));
+          }
+        });
+  }
+
+  render() {
+    return (
+      <Provider store={store}>
+        <ReduxNavigator />
+      </Provider>
+    );
+  }
+}
 
 export default Main;
