850f20a603be324b21db5352a576d78b582afa95
AuckeBos <aucke.bos@live.nl>
2017-04-12 21:22:35 +0200

    Added onclick method for events in Calendar

---
+++ app/actions/event.js
@@ -0,0 +1,4 @@
+
+export function openEvent(eventId) {
+  return null;
+}
--- app/components/Event.js
+++ app/components/Event.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import { openEvent } from '../actions/event';
 
 const Event = (event) =>
   <View>
@@ -7,7 +9,17 @@ const Event = (event) =>
     <Text> {new Date(event.start).toISOString().substring(0, 10)}</Text>
     <i>{event.description}</i>
     <Text>-----------------------------------------</Text>
+    onPress={() => openEvent(event.id)}
   </View>
 ;
 
+Event.propTypes = {
+  openEvent: React.PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => ({
+  openEvent: () => dispatch(openEvent()),
+});
+
+export default connect(() => ({}), mapDispatchToProps)(Event);
 export default Event;
