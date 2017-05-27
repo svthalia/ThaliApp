16b368e6d92eb1ed06cfa1a6214e3126fed82af5
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2017-05-27 14:03:41 +0200

    Add gradient to MemberView

--- app/components/MemberView.js
+++ app/components/MemberView.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Image, Text, ViewPropTypes, StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 import { connect } from 'react-redux';
 import styles from './style/memberView';
 import SquareView from './SquareView';
 
 const MemberView = props => (
   <SquareView style={props.style}>
-    <Image style={styles.image} source={{ uri: props.member.photo }} />
-    <Text style={styles.nameText}>{props.member.name}</Text>
+    <Image style={styles.image} source={{ uri: props.member.photo }}>
+      <LinearGradient colors={['#55000000', '#000000']} style={styles.overlayGradient} />
+      <Text style={styles.nameText}>{props.member.name}</Text>
+    </Image>
   </SquareView>
   );
 
--- app/components/style/memberView.js
+++ app/components/style/memberView.js
@@ -13,6 +13,14 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
   },
+  overlayGradient: {
+    position: 'absolute',
+    top: '50%',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    opacity: 0.6,
+  },
 });
 
 export default styles;
