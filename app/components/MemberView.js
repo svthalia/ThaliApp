883d012f097f2ab4858935308f0952423977d0b4
Gijs Hendriksen <gijs@hendrxx.nl>
2017-06-14 20:58:38 +0200

    Change profile image url to thumbnail url for MemberView

--- app/components/MemberView.js
+++ app/components/MemberView.js
@@ -3,17 +3,28 @@ import PropTypes from 'prop-types';
 import { Image, Text, ViewPropTypes, StyleSheet } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { connect } from 'react-redux';
+import { url } from '../url';
 import styles from './style/memberView';
 import SquareView from './SquareView';
 
-const MemberView = props => (
-  <SquareView style={props.style}>
-    <Image style={styles.image} source={{ uri: props.member.photo }}>
-      <LinearGradient colors={['#55000000', '#000000']} style={styles.overlayGradient} />
-      <Text style={styles.nameText}>{props.member.name}</Text>
-    </Image>
-  </SquareView>
+const MemberView = (props) => {
+  let photo = props.member.photo;
+  const regex = new RegExp(`^(${url}/media/public)/(avatars/[^\\.]+)\\.(jpg|jpeg)`);
+  const matches = regex.exec(photo);
+
+  if (matches) {
+    photo = `${matches[1]}/thumbnails/220x220_1/${matches[2]}.${matches[3]}`;
+  }
+
+  return (
+    <SquareView style={props.style}>
+      <Image style={styles.image} source={{ uri: photo }}>
+        <LinearGradient colors={['#55000000', '#000000']} style={styles.overlayGradient} />
+        <Text style={styles.nameText}>{props.member.name}</Text>
+      </Image>
+    </SquareView>
   );
+};
 
 MemberView.propTypes = {
   member: PropTypes.shape({
