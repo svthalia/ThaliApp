871e2e6223f45ff4233be6b17daae4f1b9728b43
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2017-10-03 17:04:55 +0200

    Use tokenSelector in pushNotifications saga and dispatch register action on push token refresh

--- app/actions/pushNotifications.js
+++ app/actions/pushNotifications.js
@@ -1,8 +1,8 @@
 export const REGISTER = 'PUSH_NOTIFICATIONS_REGISTER';
 export const INVALIDATE = 'PUSH_NOTIFICATIONS_INVALIDATE';
 
-export function register(token) {
-  return { type: REGISTER, payload: { token } };
+export function register() {
+  return { type: REGISTER };
 }
 
 export function invalidate() {
