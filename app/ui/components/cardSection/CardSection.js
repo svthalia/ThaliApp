2d7f4f667efc9738f83d12d88ebf307c4f8a581e
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2018-08-09 14:06:18 +0200

    Create CardSection component

---
+++ app/ui/components/cardSection/CardSection.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, Text, View, ViewPropTypes } from 'react-native';
+import styles from './style/CardSection';
+
+const CardSection = props => (
+  <View style={[styles.section, props.style]}>
+    <Text style={styles.sectionHeader}>{props.sectionHeader}</Text>
+    <View style={[styles.card, props.contentStyle]}>
+      {props.children}
+    </View>
+  </View>
+);
+
+CardSection.propTypes = {
+  children: PropTypes.node.isRequired,
+  sectionHeader: PropTypes.string,
+  style: ViewPropTypes.style,
+  contentStyle: ViewPropTypes.style,
+};
+
+const defaultStyles = StyleSheet.create({
+});
+
+CardSection.defaultProps = {
+  sectionHeader: null,
+  style: defaultStyles,
+  contentStyle: defaultStyles,
+};
+
+export default CardSection;
