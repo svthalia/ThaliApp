f11b487fb46a1baaf6e3004349ba46517c84f15b
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2018-08-09 15:06:51 +0200

    Use workaround from https://github.com/facebook/react-native/issues/19859#issuecomment-407748189 for babel issues

--- babel.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  const presets = ['react-native'];
-  const plugins = ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-regenerator'];
-  return { presets, plugins };
-};
