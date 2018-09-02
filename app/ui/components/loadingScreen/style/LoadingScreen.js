381670f552592051b316bd32ff3dfe55a9550e4f
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2018-09-02 14:04:43 +0200

    Change loadingscreen default background color

--- app/ui/components/loadingScreen/style/LoadingScreen.js
+++ app/ui/components/loadingScreen/style/LoadingScreen.js
@@ -1,10 +1,12 @@
-import { StyleSheet } from 'react-native';
+import Colors from '../../../style/Colors';
+import StyleSheet from '../../../style/StyleSheet';
 
 const styles = StyleSheet.create({
   indicatorView: {
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
+    backgroundColor: Colors.white,
   },
 });
 
