221163aad57ef0a832e14e0131ab618c1d70b742
Sébastiaan Versteeg <se_bastiaan@outlook.com>
2019-02-27 19:01:09 +0100

    Fix problems

--- __tests__/ui/components/memberView/MemberView.spec.js
+++ __tests__/ui/components/memberView/MemberView.spec.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import configureStore from 'redux-mock-store'
 import MemberView from '../../../../app/ui/components/memberView/MemberView';
-import reducer from '../../../../app/reducers/index';
 
 jest.mock('react-navigation', () => ({
   withNavigation: component => component,
 }));
 
 describe('MemberView component', () => {
-  const mockStore = configureStore(reducer);
   const initialState = {
     session: {
       token: 'token123',
     },
   };
-  const store = mockStore(initialState);
 
   const member = {
     pk: 1,
@@ -25,7 +21,7 @@ describe('MemberView component', () => {
 
   it('renders correctly', () => {
     const tree = renderer
-      .create(<MemberView store={store} member={member} size={20} />)
+      .create(<MemberView store={initialState} member={member} size={20} />)
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
