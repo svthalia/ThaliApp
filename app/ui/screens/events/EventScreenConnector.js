0dad93435aebc46453d65224c423779d869a1870
Dirk Doesburg <d.doesburg@student.science.ru.nl>
2019-10-16 19:53:01 +0200

    Fixed events map crash

--- app/ui/screens/events/EventScreenConnector.js
+++ app/ui/screens/events/EventScreenConnector.js
@@ -1,4 +1,5 @@
 import { Platform } from 'react-native';
+import { Linking } from 'react-native';
 import { connect } from 'react-redux';
 import * as pizzaActions from '../../../actions/pizza';
 import * as registrationActions from '../../../actions/registration';
@@ -18,7 +19,7 @@ const mapDispatchToProps = {
   register: registrationActions.register,
   cancel: registrationActions.cancel,
   fields: registrationActions.retrieveFields,
-  openMaps: location => this.openUrl(`https://maps.${Platform.OS === 'ios' ? 'apple' : 'google'}.com/maps?daddr=${location}`),
+  openMaps: location => Linking.openURL(`https://maps.${Platform.OS === 'ios' ? 'apple' : 'google'}.com/maps?daddr=${location}`),
   openUrl: navigationActions.openWebsite,
   retrievePizzaInfo: pizzaActions.retrievePizzaInfo,
   openAdmin: eventActions.admin,
