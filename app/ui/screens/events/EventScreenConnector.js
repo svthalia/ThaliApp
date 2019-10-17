3f00f590e3638e3aa5ccb97ca41e2565755fb6d5
Dirk Doesburg <d.doesburg@student.science.ru.nl>
2019-10-17 12:25:33 +0200

    Fix without Linking

--- app/ui/screens/events/EventScreenConnector.js
+++ app/ui/screens/events/EventScreenConnector.js
@@ -1,5 +1,4 @@
 import { Platform } from 'react-native';
-import { Linking } from 'react-native';
 import { connect } from 'react-redux';
 import * as pizzaActions from '../../../actions/pizza';
 import * as registrationActions from '../../../actions/registration';
@@ -19,7 +18,7 @@ const mapDispatchToProps = {
   register: registrationActions.register,
   cancel: registrationActions.cancel,
   fields: registrationActions.retrieveFields,
-  openMaps: location => Linking.openURL(`https://maps.${Platform.OS === 'ios' ? 'apple' : 'google'}.com/maps?daddr=${location}`),
+  openMaps: location => navigationActions.openWebsite(`https://maps.${Platform.OS === 'ios' ? 'apple' : 'google'}.com/maps?daddr=${location}`),
   openUrl: navigationActions.openWebsite,
   retrievePizzaInfo: pizzaActions.retrievePizzaInfo,
   openAdmin: eventActions.admin,
