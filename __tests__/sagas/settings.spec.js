eaff6515219342e54de5ab4b8b9339bdbea7f9f2
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-02-12 20:26:21 +0100

    Hele rare wending

--- __tests__/sagas/settings.spec.js
+++ __tests__/sagas/settings.spec.js
@@ -13,6 +13,10 @@ import * as pushNotificationActions from '../../app/actions/pushNotifications';
 import { tokenSelector } from '../../app/selectors/session';
 import { apiRequest } from '../../app/utils/url';
 
+jest.mock('react-native-snackbar', () => ({
+  Snackbar: jest.fn(),
+}));
+
 jest.mock('react-native-dotenv', () => ({
   __esModule: true,
   GITLAB_TOKEN: 'gitlab_token',
