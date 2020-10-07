13479b26585d66fdafbf83082e4a384eb60ea99c
Lars van Rhijn <larsvanrhijn@gmail.com>
2020-10-07 19:02:21 +0200

    Original babel file

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,17 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: ['module:react-native-dotenv'],
+module.exports = (api) => {
+  api.cache(false);
+  return {
+    presets: [
+      'module:react-native-dotenv',
+      'module:metro-react-native-babel-preset',
+    ],
+    plugins: [
+      '@babel/plugin-transform-flow-strip-types',
+      '@babel/plugin-proposal-object-rest-spread',
+      '@babel/plugin-proposal-optional-chaining',
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-transform-regenerator',
+      '@babel/plugin-transform-runtime',
+    ],
+  };
 };
